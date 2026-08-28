1727d90af3d4fdc9ab2381e611f250a600dc13c0
Osna-Limo <osnalimo@gmail.com>
2026-08-28 13:29:33 +0200

    Add Google OAuth client configuration placeholder

---
+++ config.js
@@ -0,0 +1,5 @@
+// Diese Client-ID ist öffentlich und kein Passwort.
+// Sobald der OAuth-Webclient in Google Cloud erstellt wurde, wird die ID hier eingetragen.
+window.OSNA_DISPO_CONFIG = {
+  googleClientId: ''
+};
